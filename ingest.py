d543a5001ef2996ae20aba0e786685351dd7d4a0
sunil_tomar <sunil_tomar@epam.com>
2026-05-13 22:40:13 +0530

    code updated for txt

--- ingest.py
+++ ingest.py
@@ -1,54 +1,57 @@
 import os
+import shutil
 
-from langchain_community.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import TextLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_ollama import OllamaEmbeddings
 from langchain_community.vectorstores import Chroma
+from langchain_ollama import OllamaEmbeddings
 
-# Folder paths
-DOCS_PATH = "docs"
 CHROMA_PATH = "chroma_db"
+DOCS_PATH = "docs"
 
-# Load embedding model
+documents = []
+
+print("Loading TXT files...")
+
+for file in os.listdir(DOCS_PATH):
+
+    if file.endswith(".txt"):
+
+        file_path = os.path.join(DOCS_PATH, file)
+
+        print("Loaded:", file)
+
+        loader = TextLoader(
+            file_path,
+            encoding="utf-8"
+        )
+
+        documents.extend(loader.load())
+
+# Split text
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=500,
+    chunk_overlap=100
+)
+
+chunks = text_splitter.split_documents(documents)
+
+print(f"Total chunks created: {len(chunks)}")
+
+# Delete old DB
+if os.path.exists(CHROMA_PATH):
+    shutil.rmtree(CHROMA_PATH)
+
+# Embedding model
 embedding = OllamaEmbeddings(
     model="nomic-embed-text"
 )
 
-# Store all documents
-all_docs = []
-
-print("Loading PDFs...")
-
-# Read all PDFs
-for file in os.listdir(DOCS_PATH):
-
-    if file.endswith(".pdf"):
-
-        pdf_path = os.path.join(DOCS_PATH, file)
-
-        loader = PyPDFLoader(pdf_path)
-
-        docs = loader.load()
-
-        all_docs.extend(docs)
-
-        print(f"Loaded: {file}")
-
-# Split documents into chunks
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,
-    chunk_overlap=200
-)
-
-chunks = text_splitter.split_documents(all_docs)
-
-print(f"Total chunks created: {len(chunks)}")
-
-# Create Chroma vector DB
-vector_db = Chroma.from_documents(
+# Create vector DB
+Chroma.from_documents(
     documents=chunks,
     embedding=embedding,
     persist_directory=CHROMA_PATH
 )
 
-print("Embeddings stored successfully in ChromaDB!")
+print("Vector DB created successfully!")
